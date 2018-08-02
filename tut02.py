b2b5d941a02fc0fb56b308bfccdddc4fd94094bc
Ian Park <ian.isu.park@gmail.com>
2018-08-02 16:39:11 +1000

    Create tut02.py

---
+++ tut02.py
@@ -0,0 +1,43 @@
+
+
+
+
+'''
+Q1. Given a list of numbers, find the median of the numbers
+You may assume that the input list contains at least one number.
+'''
+
+
+
+
+
+
+
+
+'''
+Q2. You are given a dictionary of fruits names and their prices ($)
+
+Add a new entry: 'golden apple', with a price of $42 per unit
+
+Implement a function that does the following:
+ 1. For each fruit in store, ask the user how many units
+ of the fruit the user would like to purchase (in any order)
+ 2. After the user has entered the units for every fruit,
+ calculates the total price the user has to pay and
+ prints this value out
+'''
+
+def fruit_shop(fruit_prices):
+    pass
+
+
+fruit_prices = {'bananas': 10, 'rock melons': 4, 'blueberries': 23}
+# fruit_shop(fruit_prices)
+
+
+
+
+
+'''
+Q3. Python function to find the factorial of a number provided as a parameter
+'''
